0c77c7ac7ec995b40171e2b8336adbf91f20f1d3
aldan <gitaldan@gmail.com>
2020-09-29 17:07:45 +0600

    feat: storage manipulation

--- app/static/app/index.js
+++ app/static/app/index.js
@@ -1,3 +1,4 @@
+// cookies manipulation
 function setCookie(name, value, expires) {
 
     let date = new Date();
@@ -11,19 +12,107 @@ function getCookie(name) {
     let cookies = document.cookie.split(';');
     for (let cookie of cookies) {
         cookie = cookie.trim();
-        if (cookie.indexOf(name) == 0) {
+        if (cookie.indexOf(name) === 0) {
             return cookie.substring(name.length);
         }
     }
     return '';
 }
 
-function selectSchedule(schedule) {
+// helper functions for schedule manipulation
+function selectSchedule(id) {
 
     //todo selectSchedule
 }
 
+function updateScheduleList() {
+
+    sessionStorage.setItem('scheduleList', JSON.stringify(scheduleList));
+
+    console.log('updateScheduleList: scheduleList updated');
+    console.log(scheduleList);
+}
+
+function addCourseToSchedule(schedule_id, course_id, course_sections, overwrite = false) {
+
+    for (let schedule of scheduleList) {
+
+        if (schedule.id === schedule_id) {
+
+            let course = course_id in schedule.data;
+
+            if (course === undefined || !course) {
+                // todo add to view
+            } else {
+                // todo smth
+            }
+
+            schedule.data[course_id] = course_sections;
+            break;
+        }
+    }
+
+    updateScheduleList();
+}
+
+// helper functions for course data manipulation
+function addCourseDataToStorage(id, data) {
+
+    let key = `CourseID${id}`;
+    sessionStorage.setItem(key, JSON.stringify(data));
+}
+
+function getCourseDataFromStorage(id) {
+
+    let key = `CourseID${id}`,
+        data = sessionStorage.getItem(key);
+
+    if (!data) {
+        return 0;
+    }
+
+    return JSON.parse(data);
+}
+
+function addCourseView(id) {
+
+    let data = getCourseDataFromStorage(id);
+
+    if (!data) {
+        console.log('addCourseView(): could not retrieve data');
+        return 0;
+    }
+
+    let info = semester_data.filter((course) => {
+        return course.COURSEID === id
+    })[0];
+
+    console.log('addCourseView():');
+    console.log(data);
+    console.log(info);
+
+    $('#course-list-view').append(`<li>${info.ABBR}</li>`)
+
+    return 1;
+}
+
 $(document).ready(function () {
+
+    // load schedule list from cookie
+    {
+        let schedules = sessionStorage.getItem('scheduleList');
+
+        if (!schedules) {
+            sessionStorage.setItem('scheduleList', JSON.stringify(scheduleList));
+            console.log('onReady: scheduleList initialized');
+        } else {
+            scheduleList = JSON.parse(schedules);
+            console.log('onReady: scheduleList loaded');
+        }
+
+        console.log(scheduleList);
+        selectSchedule(1);
+    }
 
     // // Search filter for Course Selector
     // $('#course-selector').on('input', function () {
@@ -64,29 +153,26 @@ $(document).ready(function () {
         select: function (event, ui) {
             let course_name = ui.item.label,
                 course_id = ui.item.value,
-                get_url = `/json?method=getCourseById&courseId=${course_id}&semesterId=${semester_code}`;
-
-            // get course data
-            let course = $.get(get_url, function (course_data) {
-                // add course if not in selected schedule
-                let selected_schedule = $('#schedule-selector option:selected').val();
-                for (let schedule of scheduleList) {
-                    if (schedule.id === selected_schedule) {
-                        let course = course_id in schedule.data;
-                        if (course === undefined || !course) {
-                            schedule.data[course_id] = [];
-                        }
-                        break;
-                    }
-                }
-
-                // todo append course to list
-                console.log(course_data);
-            });
+                get_url = `/json?method=getCourseById&courseId=${course_id}&semesterId=${semester_code}`,
+                selected_schedule_id = $('#schedule-selector option:selected').val();
 
             $(this).val('');
+            // todo loading anime
+            // get course data
+            // todo normal check if course is in schedule
+            if (getCourseDataFromStorage(course_id)) {
+                addCourseToSchedule(selected_schedule_id, course_id, [], 0);
+                addCourseView(course_id);
+                return false;
+            }
+
+            $.get(get_url, function (course_data) {
+                addCourseToSchedule(selected_schedule_id, course_id, [], 0);
+                addCourseDataToStorage(course_id, course_data);
+                addCourseView(course_id);
+            });
+
             event.preventDefault();
         }
     });
-
 });
