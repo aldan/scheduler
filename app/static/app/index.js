79e10aba1738c8d0f3589d060b4c35a261707d59
aldan <gitaldan@gmail.com>
2020-10-04 00:45:37 +0600

    feat: preload schedule, event support

--- app/static/app/index.js
+++ app/static/app/index.js
@@ -22,7 +22,25 @@ function getCookie(name) {
 // helper functions for schedule manipulation
 function selectSchedule(id) {
 
-    //todo selectSchedule
+    let schedule = scheduleList.filter((sch) => {
+        return sch.id === id.toString();
+    })[0];
+
+    let data = schedule.data;
+
+    for (const course in data) {
+
+        addCourseView(course);
+
+        let cdata = data[course];
+
+        for (const section in cdata) {
+            $(`#course-section-select-${course}-${section} option[value="${cdata[section]}"]`).prop('selected', true);
+        }
+    }
+
+    console.log(`selectSchedule: schedule '${schedule.name}' selected`)
+    updateScheduleView(id);
 }
 
 function updateScheduleList() {
@@ -33,8 +51,51 @@ function updateScheduleList() {
     console.log(scheduleList);
 }
 
-function updateScheduleView() {
+function updateScheduleView(id) {
 
+    $('.event').remove();
+
+    let schedule = scheduleList.filter((sch) => {
+        return sch.id === id.toString();
+    })[0];
+
+    let data = schedule.data;
+
+    for (const course in data) {
+
+        const cdata = data[course],
+            course_data = getCourseDataFromStorage(course),
+            [course_abbr, course_name] = $(`#course-view-${course} div:eq(0)`).html().split(': ');
+        for (const section in cdata) {
+
+            addEventToView(section, cdata[section], course_data, course_abbr);
+        }
+    }
+}
+
+function addEventToView(section, selected_section, course_data, course_abbr) {
+
+    const section_data = course_data[section].filter((sec) => {
+        return sec.code === selected_section;
+    })[0];
+
+    const tstart = section_data.starttime,
+        tend = section_data.endtime,
+        slot = parseInt(tstart / 30),
+        slot_len = (tend - tstart) / 30,
+        tstart_str = `${parseInt(tstart / 60)}:${(tstart % 60 < 10) ? '0':''}${tstart % 60}`,
+        tend_str = `${parseInt(tend / 60)}:${(tend % 60 < 10) ? '0':''}${tend % 60}`;
+
+    for (let day = 0; day < 7; day++) {
+        if (section_data.days[day]) {
+            $(`.timetable-box table tr:eq(${slot - 13}) td:eq(${day + 1})`).append(`
+                <div class="event" style="height: ${100 * slot_len}%">
+                    <b>${course_abbr}: ${selected_section}</b><br>
+                    <span style="color: rgba(255,255,255,.8)">${tstart_str} - ${tend_str}</span>
+                </div>
+            `);
+        }
+    }
 }
 
 function addCourseToSchedule(schedule_id, course_id, course_section) {
@@ -59,7 +120,9 @@ function addCourseToSchedule(schedule_id, course_id, course_section) {
     }
 
     updateScheduleList();
-    updateScheduleView();
+    if (course_section !== '') {
+        updateScheduleView(schedule_id);
+    }
 }
 
 // helper functions for course data manipulation
