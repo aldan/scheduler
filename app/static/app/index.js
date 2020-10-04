66a08fec6a58b38ecad9fdca904ca956377b75ca
aldan <gitaldan@gmail.com>
2020-10-04 16:54:47 +0600

    refactor: index.js

--- app/static/app/index.js
+++ app/static/app/index.js
@@ -1,4 +1,6 @@
-// cookies manipulation
+// index.js
+/* cookies manipulation ***********************************************************************************************/
+
 function setCookie(name, value, expires) {
 
     let date = new Date();
@@ -19,22 +21,23 @@ function getCookie(name) {
     return '';
 }
 
-// helper functions for schedule manipulation
-function selectSchedule(id) {
+/* ui manipulation ****************************************************************************************************/
 
-    let schedule = scheduleList.filter((sch) => {
+function selectSchedule(id) { /* ui: load courses from schedule with id and call updateScheduleView() */
+
+    const schedule = scheduleList.filter((sch) => {
         return sch.id === id.toString();
     })[0];
 
-    let data = schedule.data;
+    const data = schedule.data;
+    $('.course-view').remove();
 
     for (const course in data) {
 
         addCourseView(course);
+        const cdata = data[course];
 
-        let cdata = data[course];
-
-        for (const section in cdata) {
+        for (const section in cdata) { /* pre-select course sections */
             $(`#course-section-select-${course}-${section} option[value="${cdata[section]}"]`).prop('selected', true);
         }
     }
@@ -43,68 +46,110 @@ function selectSchedule(id) {
     updateScheduleView(id);
 }
 
-function updateScheduleList() {
+function updateScheduleView(id) { /* ui: add events from schedule with id, call addEventToView() */
 
-    sessionStorage.setItem('scheduleList', JSON.stringify(scheduleList));
-
-    console.log('updateScheduleList: scheduleList updated');
-    console.log(scheduleList);
-}
-
-function updateScheduleView(id) {
-
-    $('.event').remove();
-
-    let schedule = scheduleList.filter((sch) => {
+    const schedule = scheduleList.filter((sch) => {
         return sch.id === id.toString();
     })[0];
 
-    let data = schedule.data;
+    const data = schedule.data;
+    $('.event').remove();
 
     for (const course in data) {
 
         const cdata = data[course],
-            course_data = getCourseDataFromStorage(course),
+            course_data = getCourseData(course),
             [course_abbr, course_name] = $(`#course-view-${course} div:eq(0)`).html().split(': ');
-        for (const section in cdata) {
 
+        for (const section in cdata) {
             addEventToView(section, cdata[section], course_data, course_abbr);
         }
     }
 }
 
-function addEventToView(section, selected_section, course_data, course_abbr) {
+function addEventToView(section, selected_section, course_data, course_abbr) { /* ui: add event to timetable */
 
     const section_data = course_data[section].filter((sec) => {
         return sec.code === selected_section;
     })[0];
 
-    const tstart = section_data.starttime,
-        tend = section_data.endtime,
-        slot = parseInt(tstart / 30),
-        slot_len = (tend - tstart) / 30,
-        tstart_str = `${parseInt(tstart / 60)}:${(tstart % 60 < 10) ? '0':''}${tstart % 60}`,
-        tend_str = `${parseInt(tend / 60)}:${(tend % 60 < 10) ? '0':''}${tend % 60}`;
+    const time_start = section_data.starttime,
+        time_end = section_data.endtime,
+        slot = Math.trunc(time_start / 30),
+        slot_len = (time_end - time_start) / 30,
+        time_start_str = `${Math.trunc(time_start / 60)}:${(time_start % 60 < 10) ? '0' : ''}${time_start % 60}`,
+        time_end_str = `${Math.trunc(time_end / 60)}:${(time_end % 60 < 10) ? '0' : ''}${time_end % 60}`;
 
     for (let day = 0; day < 7; day++) {
         if (section_data.days[day]) {
             $(`.timetable-box table tr:eq(${slot - 13}) td:eq(${day + 1})`).append(`
                 <div class="event" style="height: ${100 * slot_len}%">
                     <b>${course_abbr}: ${selected_section}</b><br>
-                    <span style="color: rgba(255,255,255,.8)">${tstart_str} - ${tend_str}</span>
+                    <span style="color: rgba(255,255,255,.8)">${time_start_str} - ${time_end_str}</span>
                 </div>
             `);
         }
     }
 }
 
-function addCourseToSchedule(schedule_id, course_id, course_section) {
+function addCourseView(id) { /* ui: add course to the list on sidebar */
+
+    const data = getCourseData(id);
+
+    if (!data) {
+        console.log('addCourseView(): could not retrieve data');
+        return 0;
+    }
+
+    const info = semester_data.filter((course) => {
+        return course.COURSEID === id
+    })[0];
+
+    console.log('addCourseView():');
+    console.log(data);
+    console.log(info);
+
+    $('#course-list-view').append(`<div class="course-view" id="course-view-${id}"><div>${info.ABBR}: ${info.TITLE}</div></div>`);
+
+    for (const section_name in data) {
+
+        $(`#course-view-${id}`).append(`
+            <div class="course-section">
+                <label for="course-section-select-${id}-${section_name}">${section_name}</label>
+                <select id="course-section-select-${id}-${section_name}" data-course="${id}" 
+                data-section="${section_name}" onchange="updateCourseSection(this)">
+                    <option value="-1">Select section</option>
+                </select>
+            </div>
+        `);
+
+        for (const slot of data[section_name]) {
+            $(`#course-section-select-${id}-${section_name}`).append(`<option value="${slot.code}">${slot.code}</option>`);
+        }
+    }
+
+    return 1;
+}
+
+function updateCourseSection(element) { /* ui: get selected sections from view and call addCourseToSchedule() */
+
+    const selectedSectionCode = element.options[element.selectedIndex].value,
+        course_id = element.getAttribute('data-course'),
+        section = element.getAttribute('data-section'),
+        selected_schedule_id = $('#schedule-selector option:selected').val();
+
+    addCourseToSchedule(selected_schedule_id, course_id, `${section}:${selectedSectionCode}`);
+}
+
+/* schedule data manipulation *****************************************************************************************/
+
+function addCourseToSchedule(schedule_id, course_id, course_section) { /* data: add/update course data in schedule */
 
     for (let schedule of scheduleList) {
 
         if (schedule.id === schedule_id) {
 
-            let course = course_id in schedule.data;
+            const course = course_id in schedule.data;
 
             if (course === undefined || !course) {
                 schedule.data[course_id] = {};
@@ -119,25 +164,36 @@ function addCourseToSchedule(schedule_id, course_id, course_section) {
         }
     }
 
-    updateScheduleList();
-    if (course_section !== '') {
-        updateScheduleView(schedule_id);
+    storeScheduleList();
+
+    if (course_section !== '') { /* case: new entry where course data is empty */
+        updateScheduleView(schedule_id); /* call updateScheduleView() on course section update */
     }
 }
 
-// helper functions for course data manipulation
-function addCourseDataToStorage(id, data) {
+function storeScheduleList() { /* data: store schedule list in localStorage for later access in future sessions */
 
-    let key = `CourseID${id}`;
-    let val = {};
+    localStorage.setItem('scheduleList', JSON.stringify(scheduleList));
+
+    console.log('storeScheduleList: scheduleList stored to local storage');
+    console.log(scheduleList);
+}
+
+/* course data manipulation *******************************************************************************************/
+
+function storeCourseData(id, data) { /* data: store course data in localStorage to save bandwidth */
+
+    let key = `CourseID${id}`,
+        val = {};
 
     for (const section of data) {
-        let stype = section.ST,
+
+        let section_type = section.ST,
             days = [0, 0, 0, 0, 0, 0, 0],
             [starttime, endtime] = section.TIMES.split('-');
 
-        while (!isNaN(stype[0]) && stype.length > 0) {
-            stype = stype.substring(1);
+        while (!isNaN(section_type[0]) && section_type.length > 0) {
+            section_type = section_type.substring(1);
         }
 
         for (let i = 0; i < section.DAYS.length; i++) {
@@ -164,7 +220,7 @@ function addCourseDataToStorage(id, data) {
             }
         }
 
-        const convertToMins = (time12) => {
+        const convertToMins = (time12) => { /* convert time in 12hr am/pm format to minutes */
 
             let [time, ampm] = time12.split(' '),
                 [hours, mins] = time.split(':');
@@ -183,7 +239,7 @@ function addCourseDataToStorage(id, data) {
             return 60 * hours + mins;
         }
 
-        let edit_section = {
+        const edited_section = {
             code: section.ST,
             days: days,
             starttime: convertToMins(starttime),
@@ -195,20 +251,20 @@ function addCourseDataToStorage(id, data) {
         }
 
         try {
-            val[stype].push(edit_section);
+            val[section_type].push(edited_section);
         } catch (e) {
-            val[stype] = [];
-            val[stype].push(edit_section);
+            val[section_type] = [];
+            val[section_type].push(edited_section);
         }
     }
 
-    sessionStorage.setItem(key, JSON.stringify(val));
+    localStorage.setItem(key, JSON.stringify(val));
 }
 
-function getCourseDataFromStorage(id) {
+function getCourseData(id) { /* data: try to get course data from local storage, return data as object */
 
-    let key = `CourseID${id}`,
-        data = sessionStorage.getItem(key);
+    const key = `CourseID${id}`,
+        data = localStorage.getItem(key);
 
     if (!data) {
         return 0;
@@ -217,62 +273,15 @@ function getCourseDataFromStorage(id) {
     return JSON.parse(data);
 }
 
-function addCourseView(id) {
+/* onReady ************************************************************************************************************/
 
-    let data = getCourseDataFromStorage(id);
+$(document).ready(() => {
 
-    if (!data) {
-        console.log('addCourseView(): could not retrieve data');
-        return 0;
-    }
-
-    let info = semester_data.filter((course) => {
-        return course.COURSEID === id
-    })[0];
-
-    console.log('addCourseView():');
-    console.log(data);
-    console.log(info);
-
-    $('#course-list-view').append(`<div class="course-view" id="course-view-${id}"><div>${info.ABBR}: ${info.TITLE}</div></div>`);
-
-    for (const section_name in data) {
-        $(`#course-view-${id}`).append(`
-            <div class="course-section">
-                <label for="course-section-select-${id}-${section_name}">${section_name}</label>
-                <select id="course-section-select-${id}-${section_name}" data-course="${id}" 
-                data-section="${section_name}" onchange="updateCourseSection(this)">
-                    <option value="-1">Select section</option>
-                </select>
-            </div>
-        `);
-        let iterator = 0;
-        for (const slot of data[section_name]) {
-            $(`#course-section-select-${id}-${section_name}`).append(`<option value="${slot.code}">${slot.code}</option>`);
-        }
-    }
-
-    return 1;
-}
-
-function updateCourseSection(element) {
-
-    let selectedSectionCode = element.options[element.selectedIndex].value,
-        course_id = element.getAttribute('data-course'),
-        section = element.getAttribute('data-section'),
-        selected_schedule_id = $('#schedule-selector option:selected').val();
-
-    addCourseToSchedule(selected_schedule_id, course_id, `${section}:${selectedSectionCode}`);
-}
-
-$(document).ready(function () {
-
-    // load schedule list from cookie
-    {
-        let schedules = sessionStorage.getItem('scheduleList');
+    { /* load scheduleList from local storage and load default schedule */
+        const schedules = localStorage.getItem('scheduleList');
 
         if (!schedules) {
-            sessionStorage.setItem('scheduleList', JSON.stringify(scheduleList));
+            localStorage.setItem('scheduleList', JSON.stringify(scheduleList));
             console.log('onReady: scheduleList initialized');
         } else {
             scheduleList = JSON.parse(schedules);
@@ -283,35 +292,16 @@ $(document).ready(function () {
         selectSchedule(1);
     }
 
-    // // Search filter for Course Selector
-    // $('#course-selector').on('input', function () {
-    //     filteredCourseList = filtered_course_list;
-    //     // if (filtered_course_list.length <= 10) {
-    //     //     $('#filtered-course-list').empty();
-    //     //     for (const course of filtered_course_list) {
-    //     //         $('#filtered-course-list').append(`
-    //     //                     <div class="filtered-course" data-id="${course.COURSEID}" onclick="selectCourse(this)">
-    //     //                         ${course.ABBR}
-    //     //                     </div>
-    //     //                 `);
-    //     //     }
-    //     //     $('#filtered-course-list').show();
-    //     // } else {
-    //     //     $('#filtered-course-list').hide();
-    //     // }
-    // });
-
-    // Autocomplete for Course Selector
-    $('#course-selector').autocomplete({
+    $('#course-selector').autocomplete({ /* autocomplete for course selector */
         autoFocus: true,
         delay: 100,
         minLength: 2,
         source: function (req, res) {
-            let filtered_course_list = semester_data.filter(function (course) {
+            let filtered_course_list = semester_data.filter((course) => {
                 return course.ABBR.toLowerCase().indexOf(req.term.toLowerCase()) !== -1;
             });
 
-            res($.map(filtered_course_list, function (course) {
+            res($.map(filtered_course_list, (course) => {
                 return {
                     label: course.ABBR,
                     value: course.COURSEID
@@ -329,18 +319,19 @@ $(document).ready(function () {
             // todo loading anime
             // get course data
             // todo normal check if course is in schedule
-            if (getCourseDataFromStorage(course_id)) {
+            if (getCourseData(course_id)) {
                 addCourseToSchedule(selected_schedule_id, course_id, '');
                 addCourseView(course_id);
                 return false;
             }
 
-            $.get(get_url, function (course_data) {
+            // todo move get to getCourseData()
+            $.get(get_url, (course_data) => {
                 addCourseToSchedule(selected_schedule_id, course_id, '');
-                addCourseDataToStorage(course_id, course_data);
+                storeCourseData(course_id, course_data);
                 addCourseView(course_id);
             }).fail(() => {
-                alert('Registrar is unavailable')
+                alert('Registrar is unavailable');
             });
 
             event.preventDefault();
