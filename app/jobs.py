c9d0f6443f7e50da9d0954dd3457b6adbee1fcef
aldan <gitaldan@gmail.com>
2020-12-20 19:13:22 +0600

    feat(cron): convert semester_data to suitable format before updating db

--- app/jobs.py
+++ app/jobs.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Semester
 
 import logging
+import json
 import public_course_catalog as pcc
 import pdfscraper
 
@@ -38,13 +39,15 @@ class GetCourseList(CronJobBase):
             logging.error('Aborted: cur_semester_data is None')
             return
 
+        cur_semester_data = convert_csbs_data_to_dict(cur_semester_data)
+
         timestamp = timezone.now()
         logging.info('Updating data in the database.')
 
         semester_fields = {
             'semester_name': cur_semester['NAME'],
             'semester_code': cur_semester['ID'],
-            'semester_data': cur_semester_data,
+            'semester_data': json.dumps(cur_semester_data),
             'last_update_datetime': timestamp,
         }
 
@@ -59,3 +62,96 @@ class GetCourseList(CronJobBase):
             logging.error(f'Exception occurred while accessing the database\n{err.args[0]}')
         finally:
             logging.info(f'Cron job finished.\nTimestamp: {timestamp}. Time taken: {timezone.now() - start_time}')
+
+
+def convert_csbs_data_to_dict(data):
+    data = json.loads(data)
+    data = data['data']
+    del data[0]
+
+    course_dict, id_dict = {}, {}
+    cur, cid = 0, 0
+
+    for index, item in enumerate(data):
+        item = list(item.values())
+        if not item[0]:
+            prev = list(data[index - 1].values())
+            for i in range(len(item)):
+                if not item[i]:
+                    item[i] = prev[i]
+
+        if not item[0] in id_dict:
+            course_dict[cur] = {
+                'id': cur,
+                'abbr': item[0],
+                'title': item[2],
+                'credit': item[4],
+                'from': item[5],
+                'to': item[6],
+                'sections': {},
+            }
+            id_dict[item[0]] = cid = cur
+            cur += 1
+
+        section_type = item[1]
+        section_days = [0, 0, 0, 0, 0, 0, 0]
+        section_start = None
+        section_end = None
+
+        while len(section_type) and section_type[0].isdigit():
+            section_type = section_type[1:]
+
+        if item[7]:
+            for char in item[7]:
+                if not char.isalpha():
+                    pass
+                elif char.lower() == 'm':
+                    section_days[0] = 1
+                elif char.lower() == 't':
+                    section_days[1] = 1
+                elif char.lower() == 'w':
+                    section_days[2] = 1
+                elif char.lower() == 'r':
+                    section_days[3] = 1
+                elif char.lower() == 'f':
+                    section_days[4] = 1
+                elif char.lower() == 's':
+                    section_days[5] = 1
+
+        if item[8]:
+            try:
+                section_start, section_end = item[8].split('-')
+                section_start = convert_to_mins(section_start)
+                section_end = convert_to_mins(section_end)
+            except ValueError:
+                pass
+
+        section = {
+            'code': item[1],
+            'days': section_days,
+            'start': section_start,
+            'end': section_end,
+            'enrolled': int(item[9]),
+            'capacity': int(item[10]),
+            'faculty': item[11],
+            'room': item[12],
+        }
+
+        if not section_type in course_dict[cid]['sections']:
+            course_dict[cid]['sections'][section_type] = []
+        course_dict[cid]['sections'][section_type].append(section)
+
+    return course_dict
+
+
+def convert_to_mins(time12):
+    time, ampm = time12.split(' ')
+    hours, mins = map(int, time.split(':'))
+
+    if hours == 12:
+        hours = 0
+
+    if ampm.lower() == 'pm':
+        hours += 12
+
+    return 60 * hours + mins
